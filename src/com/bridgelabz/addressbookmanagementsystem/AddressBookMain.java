501dc9bc12ee4a9331f05e81acd80ac6edc17e42
Mayur <mayurzope555@gmail.com>
2020-10-06 06:18:14 +0530

    [Mayur]Added: Welcome to the Address Book Management System using Java Stream

---
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbookmanagementsystem;
+
+import java.util.*;
+class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Address Book Management System using Java Stream ");
+    }
+
+}
