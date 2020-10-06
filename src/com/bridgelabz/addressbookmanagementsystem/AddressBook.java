d1e016be3a340ec37b0bbd05da904681820b2cd1
Mayur <mayurzope555@gmail.com>
2020-10-06 06:42:32 +0530

    [Mayur]Added:  Added new contact to address book

---
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBook.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.addressbookmanagementsystem;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+
+	public ArrayList<ContactDetails> contactList = new ArrayList<>();
+
+	public  ArrayList<ContactDetails> contactList(ContactDetails cd){
+		contactList.add(cd);
+		return contactList;
+	}
+
+}
+
